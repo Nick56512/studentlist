12bc7d66de04432a48eb1f1d7e94929bf0bb5bdb
Alex <jdhshjsj>
2021-04-15 17:44:08 +0300

    Service

---
+++ StudentList/StudentList/Model/StudentService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentList.Model
+{
+    class StudentService
+    {
+        private string OpenFile()
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            if(openFile.ShowDialog()!=false)
+            {
+                return openFile.FileName;
+            }
+            return null;
+        }
+        public IEnumerable<Student> GetAll()
+        {
+            List<Student> students = new List<Student>();
+            string text = File.ReadAllText(OpenFile());
+            string[] arr = text.Split(new char[] {'\n'},StringSplitOptions.RemoveEmptyEntries);
+            foreach (var item in arr)
+            {
+                try
+                {
+                    string[] st = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    Student student = new Student();
+                    student.Name = st[0];
+                    student.Lastname = st[1];
+                    student.Age = Convert.ToInt32(st[2]);
+                    students.Add(student);
+                }
+                catch{ }
+            }
+            return students;
+        }
+        
+    }
+}
