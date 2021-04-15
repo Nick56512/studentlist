18bb4832e21c9535b2fbe0de2afb00fb969ad70d
IvanMartinyuk <martinyuk2850@gmail.com>
2021-04-15 16:54:26 +0300

    Add Student.cs

---
+++ StudentList/StudentList/Model/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentList.Model
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+    }
+}
